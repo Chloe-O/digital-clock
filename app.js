170a25ab95dc2110dc671c06127b9e6b93e78a62
Chloe-O <chloeosbourne@yahoo.co.uk>
2025-07-02 22:28:38 +0100

    storing hours,mins,secs

---
+++ app.js
@@ -0,0 +1,9 @@
+let date = new Date();
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+
+console.log(date);
+console.log(hours);
+console.log(minutes);
+console.log(seconds);
